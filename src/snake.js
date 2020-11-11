06f14857e4f0f2099f6445af2520988fc9debba4
joyact <joyleeact@gmail.com>
2020-11-12 00:11:42 +0900

    Feat: Handling the target being removed by the player

--- src/snake.js
+++ src/snake.js
@@ -2,6 +2,7 @@ import { getInputDirection } from './input.js';
 
 export const SNAKE_SPEED = 1; // snake's move per sec
 const snakeBody = [{ x: 11, y: 11 }];
+let newSegments = 0;
 
 export function update() {
   const inputDirection = getInputDirection();
@@ -23,4 +24,18 @@ export function draw(gameBoard) {
     snakeElement.classList.add('snake');
     gameBoard.appendChild(snakeElement);
   });
+}
+
+export function expandSnake(amount) {
+  newSegments += amount;
+}
+
+export function onSnake(position) {
+  return snakeBody.some(segment => {
+    return equalPositions(segment, position)
+  });
+}
+
+function equalPositions(pos1, pos2) {
+  return pos1.x === pos2.x && pos1.y === pos2.y;
 }
