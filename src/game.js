510fa63d0994d02a6c4d15023bba62911347f0b2
Joy Lee <68415905+joyact@users.noreply.github.com>
2020-11-13 21:43:20 +0900

    Feat: Failure 2 - when the snake is intersected itself

--- src/game.js
+++ src/game.js
@@ -2,7 +2,8 @@ import {
   update as updateSnake,
   draw as drawSnake,
   SNAKE_SPEED,
-  getSnakeHead
+  getSnakeHead,
+  snakeIntersection
 } from "./snake.js";
 import { update as updateFood, draw as drawFood } from "./food.js";
 import { outSideGrid } from "./grid.js";
@@ -13,7 +14,10 @@ const gameBoard = document.getElementById("game-board");
 
 function main(currentTime) {
   if (gameOver) {
-    return alert("you lose");
+    if (confirm("You lost. Press ok to restart.")) {
+      window.location = "/"; //restart the game
+    }
+    return; // game stop
   }
 
   window.requestAnimationFrame(main);
@@ -42,5 +46,5 @@ function draw() {
 }
 
 function checkDeath() {
-  gameOver = outSideGrid(getSnakeHead());
+  gameOver = outSideGrid(getSnakeHead()) || snakeIntersection();
 }
