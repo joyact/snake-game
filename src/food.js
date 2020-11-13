c9749bdbd7bdf5d884aad0fc327068c1095e3fd2
Joy Lee <68415905+joyact@users.noreply.github.com>
2020-11-13 21:07:00 +0900

    Feat: Adding segments when target is removed

--- src/food.js
+++ src/food.js
@@ -1,19 +1,19 @@
-import { onSnake, expandSnake } from './snake.js'
+import { onSnake, expandSnake } from "./snake.js";
 
-let food = { x : 10, y : 1 }
+let food = { x: 10, y: 1 };
 const EXPANSION_RATE = 1;
 
-export function update(){
-  if(onSnake(food)){
+export function update() {
+  if (onSnake(food)) {
     expandSnake(EXPANSION_RATE);
-    food = { x: 20, y: 10 }
+    food = { x: 20, y: 10 };
   }
 }
 
 export function draw(gameBoard) {
-  const foodElement = document.createElement('div');
+  const foodElement = document.createElement("div");
   foodElement.style.gridRowStart = food.y;
   foodElement.style.gridColumnStart = food.x;
-  foodElement.classList.add('food')
-  gameBoard.appendChild(foodElement)
+  foodElement.classList.add("food");
+  gameBoard.appendChild(foodElement);
 }
