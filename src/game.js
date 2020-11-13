a1ed043e3c57f9555c25eaf973df139c540253ed
Joy Lee <68415905+joyact@users.noreply.github.com>
2020-11-13 21:33:17 +0900

    Feat: Failure 1 - running off the screen

--- src/game.js
+++ src/game.js
@@ -1,10 +1,21 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js';
-import { update as updateFood, draw as drawFood } from './food.js';
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+  getSnakeHead
+} from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
+import { outSideGrid } from "./grid.js";
 
 let lastRenderTime = 0;
-const gameBoard = document.getElementById('game-board');
+let gameOver = false;
+const gameBoard = document.getElementById("game-board");
 
 function main(currentTime) {
+  if (gameOver) {
+    return alert("you lose");
+  }
+
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
@@ -21,10 +32,15 @@ window.requestAnimationFrame(main);
 function update() {
   updateSnake();
   updateFood();
+  checkDeath();
 }
 
 function draw() {
-  gameBoard.innerHTML = ''; // reset preious piece
+  gameBoard.innerHTML = ""; // reset preious piece
   drawSnake(gameBoard);
   drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outSideGrid(getSnakeHead());
 }
